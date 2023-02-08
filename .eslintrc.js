77a8466b37d11cc5be6aefc1eb428d25de2f34b2
Alexandr <caiiia_podoplelov@mail.ru>
2023-02-08 04:05:14 +0300

    initialize

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  extends: ['react-app', 'react-app/jest', 'plugin:@typescript-eslint/recommended', 'prettier'],
+
+  plugins: ['react', 'react-hooks', 'prettier'],
+  rules: {
+    'import/no-extraneous-dependencies': 'off',
+    'no-else-return': 2,
+    'no-lonely-if': 2,
+    // typescript
+    '@typescript-eslint/no-shadow': ['error'],
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    // react
+    'react/jsx-filename-extension': [
+      2,
+      {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    ],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'error',
+
+    // off
+    'import/no-unresolved': 'off',
+    'import/extensions': 'off',
+    'import/prefer-default-export': 'off',
+    'react/function-component-definition': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'no-shadow': 'off',
+    'no-unused-vars': 'off',
+    'react/require-default-props': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'object-curly-newline': 'off',
+    'no-plusplus': 'off',
+    'no-param-reassign': 'off',
+    'no-undef': 'off',
+  },
+};
